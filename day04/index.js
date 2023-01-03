6d52cb1ca513e4ee6512247ec0981dc2e87711b8
Ricardo Mestre <ricardomestre@ricardos-air-2.lan>
2023-01-03 16:54:42 +0000

    part 2 finished

--- day04/index.js
+++ day04/index.js
@@ -18,15 +18,38 @@ function isRangeWithinEachOther(range1, range2) {
   return isWithinRange(range1, range2) || isWithinRange(range2, range1);
 }
 
-async function main() {
-  const content = await readInput(FILE_INPUT);
-  const assignments = content.split("\n");
-  const fullyContained = assignments.reduce((sum, assignment) => {
-    const [range1, range2] = getRanges(assignment);
-    const inRange = isRangeWithinEachOther(range1, range2);
-    return inRange ? sum + 1 : sum;
-  }, 0);
-  console.log(fullyContained);
+function isValueWithinRange(v, r) {
+  return v >= r[0] && v <= r[r.length - 1];
 }
 
-main();
+function overlaps(range1, range2) {
+  return (
+    isValueWithinRange(range1[0], range2) ||
+    isValueWithinRange(range1[range1.length], range2) ||
+    isValueWithinRange(range2[0], range1) ||
+    isValueWithinRange(range2[range2.length], range1)
+  );
+}
+
+async function main(version = 1) {
+  const content = await readInput(FILE_INPUT);
+  const assignments = content.split("\n");
+  if (version === 1) {
+    const fullyContained = assignments.reduce((sum, assignment) => {
+      const [range1, range2] = getRanges(assignment);
+      const inRange = isRangeWithinEachOther(range1, range2);
+      return inRange ? sum + 1 : sum;
+    }, 0);
+    console.log(fullyContained);
+  } else {
+    const overlappings = assignments.reduce((sum, assignment) => {
+      const [range1, range2] = getRanges(assignment);
+      const isOverlapping = overlaps(range1, range2);
+      return isOverlapping ? sum + 1 : sum;
+    }, 0);
+    console.log(overlappings);
+  }
+}
+
+main(1);
+main(2);
