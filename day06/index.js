3351ed35f9ca32ffc5874bdf2399b5907f8f6150
Ricardo Mestre <ricardomestre@ricardos-air-2.lan>
2023-01-06 15:37:54 +0000

    part 2 finished

--- day06/index.js
+++ day06/index.js
@@ -2,9 +2,12 @@ var { readInput } = require("../helpers");
 
 const FILE_INPUT = "./day06/input.txt";
 
-function hasRepeatedChars(str) {
+const PACKET_MARKER = 4;
+const MESSAGE_MARKER = 14;
+
+function hasRepeatedChars(str, markerSize = PACKET_MARKER) {
   let found = false;
-  let idx = 3;
+  let idx = markerSize - 1;
   for (var j = 0; j < str.length - 1 && !found; j++) {
     for (var i = j + 1; i < str.length && !found; i++) {
       if (str[j] === str[i]) {
@@ -16,22 +19,25 @@ function hasRepeatedChars(str) {
   return [found, idx + 1];
 }
 
-function getMarker(content) {
+function getMarker(content, markerSize = PACKET_MARKER) {
   let pointer = 0;
   const inputLength = content.length;
   let repeatedFound = false;
   do {
-    const stream = content.slice(pointer, pointer + 4);
-    [repeatedFound, idx] = hasRepeatedChars(stream);
+    const stream = content.slice(pointer, pointer + markerSize);
+    [repeatedFound, idx] = hasRepeatedChars(stream, markerSize);
     pointer += idx;
   } while (repeatedFound && pointer < inputLength);
   return pointer;
 }
 
-async function main() {
+async function main(version = 1) {
   const content = await readInput(FILE_INPUT);
   console.log(content);
-  console.log(getMarker(content));
+  console.log(
+    getMarker(content, version === 1 ? PACKET_MARKER : MESSAGE_MARKER)
+  );
 }
 
-main();
+main(1);
+main(2);
